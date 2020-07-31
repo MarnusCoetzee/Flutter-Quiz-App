50ee04fdbbc6f061bbf86577ee7e20d06727f906
Marnus Coetzee <marnuscoe@gmail.com>
2020-07-31 22:42:18 +0200

    changes

--- lib/main.dart
+++ lib/main.dart
@@ -25,6 +25,9 @@ class QuizPage extends StatefulWidget {
 }
 
 class _QuizPageState extends State<QuizPage> {
+
+  List<Icon> scoreKeeper = [];
+
   @override
   Widget build(BuildContext context) {
     return Column(
@@ -62,6 +65,14 @@ class _QuizPageState extends State<QuizPage> {
               ),
               onPressed: () {
                 //The user picked true.
+                setState(() {
+                  scoreKeeper.add(
+                      Icon(
+                        Icons.add,
+                        color: Colors.green,
+                      )
+                  );
+                });
               },
             ),
           ),
@@ -80,21 +91,20 @@ class _QuizPageState extends State<QuizPage> {
               ),
               onPressed: () {
                 //The user picked false.
+                setState(() {
+                  scoreKeeper.add(
+                      Icon(
+                        Icons.close,
+                        color: Colors.red,
+                      )
+                  );
+                });
               },
             ),
           ),
         ),
         Row(
-          children: <Widget>[
-            Icon(
-              Icons.check,
-              color: Colors.green,
-            ),
-            Icon(
-              Icons.close,
-              color: Colors.red,
-            ),
-          ],
+          children: scoreKeeper
         ),
       ],
     );
