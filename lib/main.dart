f9277f65874f0b8a21b1873df474a2bceabc46ee
Marnus Coetzee <marnuscoe@gmail.com>
2020-08-01 13:03:54 +0200

    updated with classes

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:quizapp/question.dart';
 
 void main() => runApp(Quizzler());
 
@@ -26,7 +27,20 @@ class QuizPage extends StatefulWidget {
 
 class _QuizPageState extends State<QuizPage> {
 
-  List<Icon> scoreKeeper = [];
+  List<Question> questionBank = [
+    Question(question: 'You can lead a cow down stairs but not up stairs.', answer: false),
+    Question(question: 'Approximately one quarter of human bones are in the feet.', answer: true),
+    Question(question: 'A slug\'s blood is green.', answer: true)
+  ];
+
+  int questionNumber = 0;
+
+  void proceedNextQuestion() {
+    questionNumber++;
+    if (questionNumber > 2) {
+      questionNumber = 0;
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -40,7 +54,7 @@ class _QuizPageState extends State<QuizPage> {
             padding: EdgeInsets.all(10.0),
             child: Center(
               child: Text(
-                'This is where the question text will go.',
+                questionBank[questionNumber].questionText,
                 textAlign: TextAlign.center,
                 style: TextStyle(
                   fontSize: 25.0,
@@ -64,14 +78,17 @@ class _QuizPageState extends State<QuizPage> {
                 ),
               ),
               onPressed: () {
-                //The user picked true.
+
+                bool correctAnswer = questionBank[questionNumber].questionAnswer;
+
+                if (correctAnswer == true) {
+                  print('The User got the right answer :)');
+                } else {
+                  print('The user chose the wrong answer :(');
+                }
+
                 setState(() {
-                  scoreKeeper.add(
-                      Icon(
-                        Icons.add,
-                        color: Colors.green,
-                      )
-                  );
+                  proceedNextQuestion();
                 });
               },
             ),
@@ -90,14 +107,17 @@ class _QuizPageState extends State<QuizPage> {
                 ),
               ),
               onPressed: () {
-                //The user picked false.
+
+                bool correctAnswer = questionBank[questionNumber].questionAnswer;
+
+                if (correctAnswer == true) {
+                  print('The User got the right answer :)');
+                } else {
+                  print('The user chose the wrong answer :(');
+                }
+
                 setState(() {
-                  scoreKeeper.add(
-                      Icon(
-                        Icons.close,
-                        color: Colors.red,
-                      )
-                  );
+                  proceedNextQuestion();
                 });
               },
             ),
